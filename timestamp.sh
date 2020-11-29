7834f42a236c1ebde60b516f65a4384ca6bcf698
Student <student@TesterVM-42.edu-wsb.local>
2020-11-29 14:30:57 +0100

    dodanie czyszczenia do timestamp (prompt)

--- timestamp.sh
+++ timestamp.sh
@@ -3,3 +3,10 @@ echo "-oh no he didn't!"
 echo "oh yes he did, most recently on:"
 echo
 cat ~/custom_tools/timelog.txt | tail
+
+read -p "Czy wyczyscic ekran? [Y/N]" yn
+    case $yn in
+        [Yy]* ) clear;;
+        [Nn]* ) echo "OK, to nie czyszcze.";;
+        * ) echo "Ziomek, k**wa, proste pytanie: tak czy nie. Screw you, nie czyszcze.";;
+    esac
