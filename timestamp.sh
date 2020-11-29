cf39010e6400ed29ce33742dcf0aa73146b127c9
Student <student@TesterVM-42.edu-wsb.local>
2020-11-29 14:43:57 +0100

    typo remediation

--- timestamp.sh
+++ timestamp.sh
@@ -1,6 +1,6 @@
 date >> ~/custom_tools/timelog.txt
 echo "-oh no he didn't!"
-echo "oh yes he did, most recently on:"
+echo "-oh yes he did, most recently on:"
 echo
 cat ~/custom_tools/timelog.txt | tail
 
